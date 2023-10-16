b1c91278edc0e5d4c179fb92006db5c54d584d41
Paulius Skliaudys <paulius.skliaudys9@gmail.com>
2023-10-16 19:13:29 +0300

    dock

--- RestProject/obj/Debug/net7.0/RestProject.AssemblyInfo.cs
+++ RestProject/obj/Debug/net7.0/RestProject.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2e29c74a-e833-42bd-aa43-f456ff7beca1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
